2351734f83b21a21fe2844dd7b126bc92b543482
Ahmed Magdy <ahmedmagdy373@gmail.com>
2022-09-25 19:26:28 +0200

    ...

--- app/src/main/java/com/example/movieclub/Retrofit/Api_Interface.java
+++ app/src/main/java/com/example/movieclub/Retrofit/Api_Interface.java
@@ -1,8 +1,8 @@
 package com.example.movieclub.Retrofit;
 
+import com.example.movieclub.BuildConfig;
 import com.example.movieclub.Pojo.MovieModel.MovieModel;
 import com.example.movieclub.Pojo.MovieModel.Results;
-import com.example.movieclub.Pojo.SeriesModel.LatestSeries;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,26 +10,27 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Api_Interface {
-    @GET("discover/movie?api_key=844fc399f9e19fa60f68672ca29a948f")
+    String API_KEY = BuildConfig.API_KEY;
+    @GET("discover/movie?api_key="+API_KEY)
     public Call <MovieModel> getDiscoverMovies(
             @Query("include_adult")boolean adult,
             @Query ("sort_by")String sort
     );
-    @GET("trending/{media_type}/{time_window}?api_key=844fc399f9e19fa60f68672ca29a948f")
+    @GET("trending/{media_type}/{time_window}?api_key="+API_KEY)
     public Call <MovieModel>getTrending(
             @Path("media_type")String media_type,
             @Path("time_window")String time_window,
             @Query("include_adult")boolean adult,
             @Query("sort_by") String sort
     );
-    @GET ("movie/{movie_id}?api_key=844fc399f9e19fa60f68672ca29a948f")
+    @GET ("movie/{movie_id}?api_key="+API_KEY)
     public Call <Results> getMovie(@Path("movie_id")int movie_id);
-    @GET("movie/upcoming?api_key=844fc399f9e19fa60f68672ca29a948f")
+    @GET("movie/upcoming?api_key="+API_KEY)
     public Call <MovieModel> getUpcoming(
             @Query("include_adult")boolean adult,
             @Query ("sort_by")String sort
     );
-    @GET("movie/top_rated?api_key=844fc399f9e19fa60f68672ca29a948f")
+    @GET("movie/top_rated?api_key="+API_KEY)
     public Call <MovieModel> getTopRated(
             @Query("include_adult")boolean adult,
             @Query ("sort_by")String sort
